4ce4f797e5f8ae7dbe6dac62bcd2a05d46b47624
KaptajnKirk <theodor@duus-kirk.dk>
2022-03-11 13:27:04 +0100

    .gitignore + Test + Nej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
         System.out.println("Thomas, Morten. Kom igang! Vi skal lave collab på GitHub");
 
-        System.out.println("Nej");
+        System.out.println("Nejkkk");
     }
 }
