8e8212287be1a89c60ae6c21c3b4afdc9a3e8a5a
KaptajnKirk <theodor@duus-kirk.dk>
2022-03-11 13:01:50 +0100

    First Build

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Thomas, kom igang. Vi skal lave collab på GitHub");
+    }
+}
